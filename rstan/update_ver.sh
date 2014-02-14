3bb3ac1a2b7019bdbefd6be1aae2f1bdcf402a59
maverickg <guojq28@gmail.com>
2014-02-13 22:07:12 -0500

    udpate to 2.2.0

--- rstan/update_ver.sh
+++ rstan/update_ver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # OLD_VER=[[:digit:]]\\+\\.[[:digit:]]\\+\\.[[:digit:]]
 
-OLD_VER=2.0.1
-NEW_VER=2.1.0
+OLD_VER=2.1.1
+NEW_VER=2.2.0
 SED=gnused
 
 ${SED} -i "s/${OLD_VER}/${NEW_VER}/g" ./rstan/DESCRIPTION ./rstan/inst/CITATION ./rstan/man/rstan.Rd ./rstan/vignettes/rstan.bib 
