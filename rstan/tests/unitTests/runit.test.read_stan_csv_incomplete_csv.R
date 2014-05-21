a72bec05a436c6e51c60a10034a1799028e48f8d
Daniel Lee <bearlee@alum.mit.edu>
2014-05-20 21:52:13 -0400

    splitting tests into separate files

--- rstan/tests/unitTests/runit.test.read_stan_csv_incomplete_csv.R
+++ rstan/tests/unitTests/runit.test.read_stan_csv_incomplete_csv.R
@@ -1,6 +1,6 @@
-test_read_stan_csv <- function() { 
+test_read_stan_csv_incomplete_csv <- function() {
   exfit <- read_stan_csv(dir(system.file('misc', package = 'rstan'),
-                             'rstan_doc_ex_?.csv',
+                             'rstan_doc_ex_incomplete.csv',
                              full.names = TRUE))
   checkEquals(exfit@model_pars, c("mu", "sigma", "z", "alpha", "lp__"), checkNames = FALSE)
 }
