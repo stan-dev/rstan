89f0ae99379ba8b24918b2cc3db89eec1d5e4cd1
Daniel Lee <bearlee@alum.mit.edu>
2014-05-20 20:14:39 -0400

    updating test

--- rstan/tests/unitTests/runit.test.read_stan_csv.R
+++ rstan/tests/unitTests/runit.test.read_stan_csv.R
@@ -5,7 +5,7 @@ test_read_stan_csv <- function() {
   checkEquals(exfit@model_pars, c("mu", "sigma", "z", "alpha", "lp__"), checkNames = FALSE)
 }
 
-test_read_stan_csv_incomplete_csv() {
+test_read_stan_csv_incomplete_csv <- function() {
   exfit <- read_stan_csv(dir(system.file('misc', package = 'rstan'),
                              'rstan_doc_ex_incomplete.csv',
                              full.names = TRUE))
