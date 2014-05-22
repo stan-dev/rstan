a639d68400ce5795f7f50df9e1268b2dfc137a39
Daniel Lee <bearlee@alum.mit.edu>
2014-05-22 10:09:23 -0400

    updating pattern rules for tests

--- rstan/tests/unitTests/runit.test.read_stan_csv.R
+++ rstan/tests/unitTests/runit.test.read_stan_csv.R
@@ -1,6 +1,6 @@
 test_read_stan_csv <- function() { 
   exfit <- read_stan_csv(dir(system.file('misc', package = 'rstan'),
-                             'rstan_doc_ex_?.csv',
+                             pattern='rstan_doc_ex_[[:digit:]].csv',
                              full.names = TRUE))
   checkEquals(exfit@model_pars, c("mu", "sigma", "z", "alpha", "lp__"), checkNames = FALSE)
 }
--- rstan/tests/unitTests/runit.test.read_stan_csv_incomplete_csv.R
+++ rstan/tests/unitTests/runit.test.read_stan_csv_incomplete_csv.R
@@ -1,6 +1,6 @@
 test_read_stan_csv_incomplete_csv <- function() {
   exfit <- read_stan_csv(dir(system.file('misc', package = 'rstan'),
-                             'rstan_doc_ex_incomplete.csv',
+                             pattern='rstan_doc_ex_[[:alpha:]]+.csv',
                              full.names = TRUE))
   checkEquals(exfit@model_pars, c("mu", "sigma", "z", "alpha", "lp__"), checkNames = FALSE)
 }
