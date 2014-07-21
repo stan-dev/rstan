587e65b764ab9e49dd0f8207c072055a94ca1fd5
maverickg <guojq28@gmail.com>
2014-07-20 21:34:48 -0400

    put git SHA into ontacch again

--- rstan/rstan/R/zzz.R
+++ rstan/rstan/R/zzz.R
@@ -4,8 +4,7 @@
   rstanLib <- dirname(system.file(package = "rstan"))
   pkgdesc <- packageDescription("rstan", lib.loc = rstanLib)
   builddate <- gsub(';.*$', '', pkgdesc$Packaged)
-#  gitrev <- substring(git_head(), 0, 12)
-  gitrev <- "CRAN"
+  gitrev <- substring(git_head(), 0, 12)
   packageStartupMessage(paste("rstan (Version ", pkgdesc$Version, ", packaged: ", builddate, ", GitRev: ", gitrev, ")", sep = ""))
 } 
 
