stan-dev/rstan

commit 28784f4c4e45dbfaae7fc43e2614f3d886de1d44

add more warning messages for passing wrong arguments

rstan/rstan/R/misc.R
@@ -411,10 +411,21 @@ config_argss <- function(chains, iter, warmup, thin,
   all_metrics <- c("unit_e", "diag_e", "dense_e")
   if (!is.null(control)) {
     if (!is.list(control)) 
-      stop("control should be a named list")
+      stop("'control' should be a named list")
+      is_arg_recognizable(names(control),
+                          c("adapt_engaged", "adapt_gamma",
+                            "adapt_delta", "adapt_kappa", "adapt_t0", 
+                            "adapt_init_buffer", "adapt_term_buffer",
+                            "adapt_window", "stepsize", 
+                            "stepsize_jitter", "metric", "int_time",
+                            "max_treedepth", 
+                            "epsilon", "error"),
+                          pre_msg = "'control' list contains unknown members of names: ",
+                          call. = FALSE)
     metric <- control$metric
-    if (!is.null(metric))
-      control$metric <- match.arg(metric, all_metrics)
+    if (is.null(metric) || is.na(match(metric, all_metrics))) {
+      stop("metric should be one of ", paste0(paste0('"', all_metrics, '"'), collapse = ", "))
+    }
     dotlist$control <- control
   } 
 
@@ -1470,14 +1481,15 @@ read_csv_header <- function(f, comment.char = '#') {
   header
 }
 
-is_arg_recognizable <- function(x, y) {
+is_arg_recognizable <- function(x, y, pre_msg = '', post_msg = '', ...) {
   # check if all elements of x are in y.  
   # x: a vector of characters 
   # y: a vector of characters 
   idx <- match(x, y)
   na_idx <- which(is.na(idx))
   if (length(na_idx) > 0) {
-    warning(paste(x[na_idx], collapse = ', '), " not found.")
+    warning(pre_msg, paste(x[na_idx], collapse = ', '), ".", post_msg, ...)
+    return(FALSE)
   }
   TRUE
 }

rstan/example/test8schools.R
@@ -1,7 +1,7 @@
 library(rstan)
 
 model_name <- "_8chools";
-sfile <- "../../stan/src/models/misc/eight_schools/eight_schools.stan"
+sfile <- "../../example-models/misc/eight_schools/eight_schools.stan"
 m <- stan_model(file = sfile, 
                 model_name = model_name, 
                 verbose = TRUE)  

rstan/example/testargs.R
@@ -1,0 +1,14 @@
+library(rstan)
+sm <- stan_model(model_code = 'data { int N; } parameters { real y[N]; } model {y ~ normal(0, 1); }')
+
+a <- sampling(sm, data = list(N = 3))
+
+b <- sampling(sm, controls = list(metric = "d"))
+
+c <- sampling(sm, control = list(metric = "e"))
+
+c2 <- sampling(sm, data = list(N = 3), control = list(metric = "e"))
+
+c3 <- sampling(sm, data = list(N = 3), control = list(unmetric = "e"))
+
+d <- stan(fit = a, unkown_arg1 = 1, unkown_arg2 = 2, data = list(N = 4))

rstan/example/testoptim.R
@@ -20,7 +20,7 @@ y <- rnorm(N)
 cat("mean(y)=", mean(y), ", and sd(y)=", sd(y), "\n", sep = '')
 dat <- list(N = N, y = y)
 dump(c("N", "y"), file = 'optim.data.R')
-sm <- stan_model(model_code = stdnorm)
+sm <- stan_model(model_code = stdnorm, verbose = TRUE)
 
 
 # default algorithm 

rstan/rstan/R/rstan.R
@@ -123,7 +123,9 @@ stan <- function(file, model_name = "anon_model",
   is_arg_recognizable(dot_arg_names, 
                       c("chain_id", "init_r", "test_grad", 
                         "append_samples", "refresh", "control",
-                        "obfuscate_model_name"))
+                        "obfuscate_model_name"),
+                      pre_msg = "passing unknown arguments: ", 
+                      call. = FALSE)
 
   if (is(fit, "stanfit")) sm <- get_stanmodel(fit)
   else { 

rstan/rstan/R/stanmodel-class.R
@@ -113,7 +113,9 @@ setMethod("optimizing", "stanmodel",
                                   "tol_param",
                                   "tol_rel_obj",
                                   "tol_rel_grad",
-                                  "history_size"))
+                                  "history_size"),
+                                 pre_msg = "passing unknown arguments: ",
+                                 call. = FALSE)
             if (!is.null(dotlist$method))  dotlist$method <- NULL
             optim <- sampler$call_sampler(c(args, dotlist))
             names(optim$par) <- flatnames(m_pars, p_dims, col_major = TRUE)
@@ -150,7 +152,9 @@ setMethod("sampling", "stanmodel",
               is_arg_recognizable(names(dots),
                                   c("chain_id", "init_r", "test_grad",
                                     "obfuscate_model_name",
-                                    "append_samples", "refresh", "control"))
+                                    "append_samples", "refresh", "control"), 
+                                  pre_msg = "passing unknown arguments: ",
+                                  call. = FALSE)
             }
             prep_call_sampler(object)
             model_cppname <- object@model_cpp$model_cppname 

commit 9d35ddcbc0a97f47a871a2bdee863ed178721bf7

fix a bug introduced in 28784f4c4e45dbfaae7fc43e2614f3d886de1d44

rstan/rstan/R/misc.R
@@ -423,7 +423,7 @@ config_argss <- function(chains, iter, warmup, thin,
                           pre_msg = "'control' list contains unknown members of names: ",
                           call. = FALSE)
     metric <- control$metric
-    if (is.null(metric) || is.na(match(metric, all_metrics))) {
+    if (!is.null(metric) && is.na(match(metric, all_metrics))) {
       stop("metric should be one of ", paste0(paste0('"', all_metrics, '"'), collapse = ", "))
     }
     dotlist$control <- control