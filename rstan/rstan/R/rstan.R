f6ae20e5e332a23e475a076363467714e66550c4
Alexey Stukalov <astukalov@gmail.com>
2015-05-01 22:02:57 +0200

    R CMD config CXX: call the same R as in session
    call $RHOME/bin/R, don't assume that the R we need is located within
    the default $PATHs

--- rstan/rstan/R/rstan.R
+++ rstan/rstan/R/rstan.R
@@ -96,7 +96,8 @@ stan_model <- function(file,
     on.exit(rstan_options(eigen_lib = old.eigen_lib), add = TRUE) 
   }
 
-  check <- system2("R", args = "CMD config CXX", 
+  check <- system2(file.path(R.home(component = "bin"), "R"),
+                   args = "CMD config CXX", 
                    stdout = TRUE, stderr = FALSE)
   if(identical(check, "")) {
     WIKI <- "https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started"
