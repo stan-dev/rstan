ae7e3b7f67a96fdc950005297bbccecc3aa1f062
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-09-10 11:33:32 -0400

    Working sparse extractor.

--- rstan/rstan/src/sparse_extractors.cpp
+++ rstan/rstan/src/sparse_extractors.cpp
@@ -8,12 +8,13 @@
 
 RcppExport SEXP extract_sparse_components(SEXP A) {
   Eigen::SparseMatrix<double> AA(Rcpp::as<Eigen::SparseMatrix<double> >(A));
-  Eigen::Matrix<double, Eigen::Dynamic,1> w = stan::math::csr_extract_w(AA);
+  Eigen::SparseMatrix<double, Eigen::RowMajor> BB(AA.transpose());
+  Eigen::Matrix<double, Eigen::Dynamic, 1> w = stan::math::csr_extract_w(BB);
   std::vector<double> ww(w.size());
   for (unsigned int i=0; i < w.size(); ++i) ww[i] = w.coeff(i);
 
-  std::vector<int> v = stan::math::csr_extract_v(AA);
-  std::vector<int> u = stan::math::csr_extract_u(AA);
+  std::vector<int> v = stan::math::csr_extract_v(BB);
+  std::vector<int> u = stan::math::csr_extract_u(BB);
 
   return Rcpp::List::create(
     Rcpp::Named("w") = ww,
