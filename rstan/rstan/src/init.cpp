f4609c78cc1a7a0e4e58b89fe04418d330a61358
Ben Goodrich <goodrich.ben@gmail.com>
2015-09-16 09:23:37 -0400

    bump version numbers for 2.8.0 release

--- rstan/rstan/src/init.cpp
+++ rstan/rstan/src/init.cpp
@@ -39,7 +39,7 @@ SEXP seq_permutation(SEXP conf);
 SEXP CPP_read_comments(SEXP file, SEXP n);
 SEXP stan_prob_autocovariance(SEXP v);
 SEXP is_Null_NS(SEXP ns);
-SEXP CPP_stanc261(SEXP model_stancode, SEXP model_name);
+SEXP CPP_stanc280(SEXP model_stancode, SEXP model_name);
 SEXP CPP_stan_version();
 #ifdef __cplusplus
 }
@@ -56,7 +56,7 @@ static const R_CallMethodDef CallEntries[] = {
   CALLDEF(CPP_read_comments, 2),
   CALLDEF(stan_prob_autocovariance, 1),
   CALLDEF(is_Null_NS, 1),
-  CALLDEF(CPP_stanc261, 2),
+  CALLDEF(CPP_stanc280, 2),
   CALLDEF(CPP_stan_version, 0),
   {NULL, NULL, 0}
 };
