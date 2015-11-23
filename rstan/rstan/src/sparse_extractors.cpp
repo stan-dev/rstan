965a42b660a9a1bc6a3c51a87fd59d6af2cfa2b2
Ben Goodrich <goodrich.ben@gmail.com>
2015-11-23 08:33:07 -0500

    sparse_extractors.cpp: fix unsigned vs signed comparison

--- rstan/rstan/src/sparse_extractors.cpp
+++ rstan/rstan/src/sparse_extractors.cpp
@@ -11,7 +11,7 @@ RcppExport SEXP extract_sparse_components(SEXP A) {
   Eigen::SparseMatrix<double, Eigen::RowMajor> BB(AA.transpose());
   Eigen::Matrix<double, Eigen::Dynamic, 1> w = stan::math::csr_extract_w(BB);
   std::vector<double> ww(w.size());
-  for (unsigned int i=0; i < w.size(); ++i) ww[i] = w.coeff(i);
+  for (int i=0; i < w.size(); ++i) ww[i] = w.coeff(i);
 
   std::vector<int> v = stan::math::csr_extract_v(BB);
   std::vector<int> u = stan::math::csr_extract_u(BB);
