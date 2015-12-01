9797a135255a8f4b9ee87be8fb99645d0cd7c642
Ben Goodrich <goodrich.ben@gmail.com>
2015-12-01 01:54:37 -0500

    renable vb

--- rstan/rstan/R/stan_demo.R
+++ rstan/rstan/R/stan_demo.R
@@ -22,7 +22,7 @@ delete_stan_demo_folder <- function() {
 
 stan_demo <-
 function(model = character(0), 
-         method = c("sampling", "optimizing"), ...) {
+         method = c("sampling", "optimizing", "meanfield", "fullrank"), ...) {
   if(is.numeric(model)) {
     MODEL_NUM <- as.integer(model)
     model <- character(0)
@@ -82,5 +82,5 @@ function(model = character(0),
   dots$data <- STAN_ENV
   if(method == "sampling") return(do.call(sampling, args = dots))
   else if (method == "optimizing") return(do.call(optimizing, args = dots))
-  # else return(do.call(vb, args = dots))
+  else return(do.call(vb, args = dots))
 }
