30f0ace117c2e9737f34cee285c43ebfd418d13c
Ben Goodrich <goodrich.ben@gmail.com>
2016-01-02 16:14:59 -0500

    bump to 2.9.0

--- rstan/update_ver.sh
+++ rstan/update_ver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # OLD_VER=[[:digit:]]\\+\\.[[:digit:]]\\+\\.[[:digit:]]
 
-OLD_VER=2.7.0
-NEW_VER=2.8.0
+OLD_VER=2.8.9
+NEW_VER=2.9.0
 SED=sed
 
 ${SED} -i "s/${OLD_VER}/${NEW_VER}/g" ./rstan/DESCRIPTION ./rstan/inst/CITATION ./rstan/man/rstan.Rd ./rstan/vignettes/rstan.bib
@@ -12,5 +12,5 @@ NEW_VER2=${NEW_VER//./}
 
 ${SED} -i "s/stanc${OLD_VER2}/stanc${NEW_VER2}/g" ./rstan/R/stanc.R ./rstan/src/init.cpp ./rstan/src/stanc.cpp
 
-echo "use git diff to double check" 
+echo "use git diff to double check"
 echo "please change dates in DESCRIPTION and man/rstan.md"
