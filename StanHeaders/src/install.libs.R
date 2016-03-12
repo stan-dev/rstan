be053cecf7721ee97391d56a95966cf87bf575c1
Ben Goodrich <goodrich.ben@gmail.com>
2016-03-12 14:31:32 -0500

    StanHeaders: Get working on Windows
    but we have warnings from R CMD check

--- StanHeaders/src/install.libs.R
+++ StanHeaders/src/install.libs.R
@@ -1,8 +1,16 @@
-files <- Sys.glob("../lib/libStanHeaders.a")
-stopifnot(length(files) > 0)
-dest <- file.path(R_PACKAGE_DIR, paste0('lib', R_ARCH))
-dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-file.copy(files, dest, overwrite = TRUE)
-if (file.exists("symbols.rds"))
-  file.copy("symbols.rds", dest, overwrite = TRUE)
+if (.Platform$OS.type == "unix") {
+  files <- Sys.glob("../lib/libStanHeaders.a")  
+  dest <- file.path(R_PACKAGE_DIR, paste0('lib', R_ARCH))
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest, overwrite = TRUE)
+  if (file.exists("symbols.rds"))
+    file.copy("symbols.rds", dest, overwrite = TRUE)
+} else {
+  files <- Sys.glob(paste0("*", SHLIB_EXT))
+  dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest, overwrite = TRUE)
+  if(file.exists("symbols.rds"))
+    file.copy("symbols.rds", dest, overwrite = TRUE)  
+}
 
