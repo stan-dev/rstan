d9a2c46214c733d6380bad1d5ae7d108302d3a01
jgabry <jgabry@gmail.com>
2016-05-14 18:29:30 -0400

    for plots, only allow pars from parameters block if unconstrain=TRUE

--- rstan/rstan/R/stan_plot_helpers.R
+++ rstan/rstan/R/stan_plot_helpers.R
@@ -154,6 +154,19 @@ is.stanfit <- function(x) inherits(x, "stanfit")
     out
   })
   nchains <- length(samp_use)
+  
+  if (unconstrain) {
+    if (is.stanreg(object)) 
+      object <- object$stanfit
+    pblock <- .get_stan_params(object)
+    pars2 <- unlist(lapply(strsplit(pars, "\\["), "[[", 1))
+    not_pblock <- length(setdiff(unique(pars2), pblock))
+    if (not_pblock)
+      stop("If 'unconstrain' is TRUE only variables declared in the ",
+           "'parameters' block can be included in 'pars'.", 
+           call. = FALSE)
+  }
+  
   dat <- .reshape_sample(samp_use)
   dat$iteration <- idx
   dat$chain <- factor(dat$chain)
@@ -170,8 +183,19 @@ is.stanfit <- function(x) inherits(x, "stanfit")
        warmup = warmup)
 }
 
+# get parameter names for parameters block only
+.get_stan_params <- function(object) {
+  stopifnot(is.stanfit(object))
+  params <- grep("context__.vals_r", fixed = TRUE, value = TRUE, 
+                 x = strsplit(get_cppcode(get_stanmodel(object)), "\n")[[1]])
+  params <- sapply(strsplit(params, "\""), FUN = function(x) x[[2]])
+  intersect(params, object@model_pars)
+}
+
+# unconstrain
 .upars <- function(object, permuted = FALSE, inc_warmup = TRUE) {
-  if (is.stanreg(object)) object <- object$stanfit
+  if (is.stanreg(object)) 
+    object <- object$stanfit
   pars <- extract(object, permuted = permuted, inc_warmup = inc_warmup)
   nchains <- ncol(pars)
   pn <- dimnames(pars)$parameters
@@ -186,6 +210,16 @@ is.stanfit <- function(x) inherits(x, "stanfit")
     upars <- aperm(upars, c(2, 3, 1))
   }
   else upars <- aperm(upars, c(3, 1, 2))
+  
+  pblock <- .get_stan_params(object)
+  mark <- c()
+  for (p in pblock) {
+    patt <- paste0("^", p, "|^", p, "\\[")
+    sel <- grep(patt, param_names)  
+    if (length(sel))
+      mark <- c(mark, sel)
+  }
+  param_names <- param_names[sort(mark)]
   
   lapply(seq_len(nchains), function(chain) {
     x <- upars[chain,, ]
