a0381efb0b584862e24161405f29e6b3dcfee600
Ben Goodrich <goodrich.ben@gmail.com>
2016-06-18 20:23:42 -0400

    rstan: version is 2.10.0

--- rstan/update_ver.sh
+++ rstan/update_ver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # OLD_VER=[[:digit:]]\\+\\.[[:digit:]]\\+\\.[[:digit:]]
 
-OLD_VER=2.9.1
-NEW_VER=2.9.10
+OLD_VER=2.9.10
+NEW_VER=2.10.0
 SED=sed
 
 ${SED} -i "s/${OLD_VER}/${NEW_VER}/g" ./rstan/DESCRIPTION ./rstan/inst/CITATION ./rstan/man/rstan.Rd ./rstan/vignettes/rstan.bib
