6f9ec2e2ce9d3816e9fe23e492c0e89e4cd99916
Ben Goodrich <goodrich.ben@gmail.com>
2016-06-21 23:55:11 -0400

    fix copyrights

--- rstan/rstan/src/init.cpp
+++ rstan/rstan/src/init.cpp
@@ -1,5 +1,5 @@
 // This file is part of RStan
-// Copyright (C) 2012, 2013, 2014, 2015 Jiqiang Guo and Benjamin Goodrich
+// Copyright (C) 2012, 2013, 2014, 2015 Trustees of Columbia University
 //
 // RStan is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
