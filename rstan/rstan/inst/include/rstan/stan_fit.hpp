193974038f29ec6f5318cea013e972a4be8cdb6d
Daniel Lee <bearlee@alum.mit.edu>
2016-08-29 10:53:10 -0400

    Adding a thinner stan_fit.hpp

---
+++ rstan/rstan/inst/include/rstan/stan_fit.hpp
@@ -0,0 +1,23 @@
+#ifndef RSTAN__STAN_FIT_HPP
+#define RSTAN__STAN_FIT_HPP
+
+#include <Rcpp.h>
+#include <rstan/io/rlist_ref_var_context.hpp>
+
+namespace rstan {
+
+  template <class Model>
+  class stan_fit {
+  private: 
+    io::rlist_ref_var_context data_;
+    Model model_;
+    Rcpp::Function cxxfunction; // keep a reference to the cxxfun, no functional purpose.
+
+    stan_fit(SEXP data, SEXP cxxf) :
+      data_(data),
+      model_(data_, &rstan::io::rcout),
+      cxxfunction(cxxf) {
+    }
+  };
+
+}
