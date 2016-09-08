7fe44f21cc2957f9fbe9d93b29c5f6ed29ba5c46
Daniel Lee <bearlee@alum.mit.edu>
2016-09-08 14:16:16 -0400

    Adding a diagnose method

--- rstan/rstan/inst/include/rstan/stan_model_with_data.hpp
+++ rstan/rstan/inst/include/rstan/stan_model_with_data.hpp
@@ -22,20 +22,13 @@ namespace rstan {
         model_(data_, &rstan::io::rcout) {
     }
     
-    int diagnose() {
-      // set up call
-      // call
-      stan::io::empty_var_context init;
-      unsigned int random_seed = 0;
-      unsigned int chain = 1;
-      double init_radius = 2;
-      double epsilon = 1e-6;
-      double error = 1e-6;
+    int diagnose(const Rcpp::List& init_list, int random_seed, int chain, double init_radius, double epsilon, double error) {
+      io::rlist_ref_var_context init(init_list);
       stan::callbacks::noop_interrupt interrupt;
       stan::callbacks::noop_writer message_writer;
       stan::callbacks::noop_writer init_writer;
       stan::callbacks::stream_writer parameter_writer(rstan::io::rcout);
-      
+      rstan::io::rcout << "init contains y: " << init.contains_r("y") << std::endl;
       return stan::services::diagnose::diagnose(model_,
                                                 init,
                                                 random_seed,
