813ea057ec72cb7383533543e5428ae8ec990a2a
Daniel Lee <bearlee@alum.mit.edu>
2016-10-12 19:45:17 -0400

    Removed unused lines of code

--- rstan/rstan/inst/include/rstan/rstan_writer.hpp
+++ rstan/rstan/inst/include/rstan/rstan_writer.hpp
@@ -113,11 +113,6 @@ namespace rstan {
      * @param[in] state Values in a std::vector
      */
     void operator()(const std::vector<double>& state) {
-      // FIXME(dl): remove
-      //Rcpp::Rcout << "next iter(" << state.size() << "): ";
-      //for (size_t n = 0; n < state.size(); n++)
-      //Rcpp::Rcout << state[n] << " ";
-      //Rcpp::Rcout << std::endl;
       csv_(state);
       values_(state);
       sampler_values_(state);
@@ -175,27 +170,6 @@ namespace rstan {
     for (size_t n = 0; n < offset; n++)
       filter_sampler_values[n] = n;
 
-    // FIXME(dl): remove
-    // Rcpp::Rcout << "N = " << N << std::endl
-    //             << "N_iter_save = " << N_iter_save << std::endl
-    //             << "offset = " << offset << std::endl;
-
-    
-    // Rcpp::Rcout << "lp(" << lp.size() << "): ";
-    // for (size_t n = 0; n < lp.size(); ++n)
-    //   Rcpp::Rcout << lp[n] << " ";
-    // Rcpp::Rcout << std::endl;
-
-    // Rcpp::Rcout << "filter(" << filter.size() << "): ";
-    // for (size_t n = 0; n < filter.size(); ++n)
-    //   Rcpp::Rcout << filter[n] << " ";
-    // Rcpp::Rcout << std::endl;
-
-    // Rcpp::Rcout << "filter_sampler_values(" << filter_sampler_values.size() << "): ";
-    // for (size_t n = 0; n < filter_sampler_values.size(); ++n)
-    //   Rcpp::Rcout << filter_sampler_values[n] << " ";
-    // Rcpp::Rcout << std::endl;
-    
     stan::callbacks::stream_writer csv(*o, prefix);
     filtered_values<Rcpp::NumericVector> values(N, N_iter_save, filter);
     filtered_values<Rcpp::NumericVector> sampler_values(N, N_iter_save, filter_sampler_values);
