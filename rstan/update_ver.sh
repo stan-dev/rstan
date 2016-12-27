ee56896cbfaa684bfcd897441fc431a0a965258f
Ben Goodrich <goodrich.ben@gmail.com>
2016-12-27 09:24:59 -0500

    bump to 2.14.x

--- rstan/update_ver.sh
+++ rstan/update_ver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # OLD_VER=[[:digit:]]\\+\\.[[:digit:]]\\+\\.[[:digit:]]
 
-OLD_VER=2.12.1
-NEW_VER=2.13.1
+OLD_VER=2.13.2
+NEW_VER=2.14.1
 SED=sed
 
 ${SED} -i "s/${OLD_VER}/${NEW_VER}/g" ./rstan/DESCRIPTION ./rstan/inst/CITATION ./rstan/man/rstan.Rd ./rstan/vignettes/rstan.bib
