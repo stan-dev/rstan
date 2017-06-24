aeec165b5f8199f45e67bd60af93d337f0c08250
Ben Goodrich <goodrich.ben@gmail.com>
2017-06-24 15:39:56 -0400

    StanHeaders/src/init.c: actually commit this file

---
+++ StanHeaders/src/init.c
@@ -0,0 +1,15 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
+#include <Rversion.h>
+
+static const R_CallMethodDef CallEntries[] = {
+  {NULL, NULL, 0}
+};
+
+void attribute_visible R_init_rstanarm(DllInfo *dll) {
+  // next line is necessary to avoid a NOTE from R CMD check
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, TRUE); // necessary for .onLoad() to work
+}
