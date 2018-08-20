dabdbfd9bbfe524ce7c0854aecd7c0efa53fc901
Ben Goodrich <goodrich.ben@gmail.com>
2018-08-20 12:35:25 -0400

    expose_stan_functions: work around for functions ending in _lp

--- rstan/rstan/R/expose_stan_functions.R
+++ rstan/rstan/R/expose_stan_functions.R
@@ -19,6 +19,9 @@ expose_stan_functions_hacks <- function(code, includes = NULL) {
   code <- paste("#include <exporter.h>\n#include <RcppEigen.h>", code, sep="\n")
   code <- gsub("// [[stan::function]]", 
                "// [[Rcpp::depends(rstan)]]\n// [[Rcpp::export]]", code, fixed = TRUE)
+  code <- gsub("stan::math::accumulator<double>& lp_accum__, std::ostream* pstream__ = nullptr){", 
+               "std::ostream* pstream__ = nullptr){\nstan::math::accumulator<double> lp_accum__;", 
+               code, fixed = TRUE)
   if(is.null(includes)) return(code)
   code <- sub("\n\nstan::io::program_reader prog_reader__() {",
               paste0("\n", includes, "\nstan::io::program_reader prog_reader__() {"), 
@@ -56,6 +59,7 @@ expose_stan_functions <- function(stanmodel, includes = NULL, ...) {
     FUN <- get(x, envir = ENV)
     args <- formals(FUN)
     args$pstream__ <- get_stream()
+    if ("lp__" %in% names(args)) args$lp__ <- 0
     if ("base_rng__" %in% names(args)) args$base_rng__ <- get_rng()
     formals(FUN) <- args
     assign(x, FUN, envir = ENV)
