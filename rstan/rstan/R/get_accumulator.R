a42c33375040dd89b335c7592020d90626eb778a
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2018-08-20 20:32:31 -0400

    That's most of the code.

---
+++ rstan/rstan/R/get_accumulator.R
@@ -0,0 +1,10 @@
+get_rng <- function(start=0L) {
+  if (start != 0L) {
+    if (length(start) != 1) 
+      stop("Start value must be a length-1 integer vector.")
+  }
+  return(.Call('get_accumulator_', start))
+}
+
+
+
--- rstan/rstan/src/pointer-tools.cpp
+++ rstan/rstan/src/pointer-tools.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include <boost/random/additive_combine.hpp>
+#include <stan/math/prim/math/fun/accumulator.hpp>
 
 RcppExport SEXP get_stream_() {
   std::ostream* pstream(&Rcpp::Rcout);
@@ -11,6 +12,13 @@ RcppExport SEXP get_rng_(SEXP seed) {
   int seed_ = Rcpp::as<int>(seed);
   boost::ecuyer1988* rng = new boost::ecuyer1988(seed_);
   Rcpp::XPtr<boost::ecuyer1988> ptr(rng, true);
+  return ptr;
+}
+
+RcppExport SEXP get_accumulator_(SEXP start) {
+  int start_ = Rcpp::as<int>(start);
+  stan::math::accumulator<double>* acc = new stan::math::accumulator<double>(start_);
+  Rcpp::XPtr<stan::math::accumulator<double>> ptr(acc, true);
   return ptr;
 }
 
