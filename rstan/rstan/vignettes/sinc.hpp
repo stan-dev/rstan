67522909941d80acf74d1e19b5dbd050a6141e42
Ben Goodrich <goodrich.ben@gmail.com>
2018-10-18 22:41:28 -0400

    sinc.hpp: Fix for vignette

--- rstan/rstan/vignettes/sinc.hpp
+++ rstan/rstan/vignettes/sinc.hpp
@@ -1,14 +1,13 @@
-template <typename T0__>
-T0__
-sinc(const T0__& x, std::ostream* pstream__) {
-  return x != 0.0 ? sin(x) / x : (x + 1.0);
+double
+sinc(const double& x, std::ostream* pstream__) {
+  return x != 0.0 ? sin(x) / x : 1.0;
 }
 
-stan::math::vari
-sinc(const stan::math::var& x, std::ostream* pstream__) {
+var
+sinc(const var& x, std::ostream* pstream__) {
   double x_ = x.val();
   double f = x_ != 0.0 ? sin(x_) / x_ : 1.0;
   double dfdx_ = x_ != 0.0 ? (cos(x_) - sin(x_)) / x_ : 0.0;
-  return precomp_v_vari(f, x.vi_, dfdx_);
+  return precomputed_gradients(f, {x.vi_}, {dfdx_});
 }
 
