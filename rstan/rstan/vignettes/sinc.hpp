c3fa74e02af8083a30913b8f7d5f200c126587bd
Ben Goodrich <goodrich.ben@gmail.com>
2018-10-28 13:20:58 -0400

    external.Rmd: update

--- rstan/rstan/vignettes/sinc.hpp
+++ rstan/rstan/vignettes/sinc.hpp
@@ -8,6 +8,6 @@ sinc(const var& x, std::ostream* pstream__) {
   double x_ = x.val();
   double f = x_ != 0.0 ? sin(x_) / x_ : 1.0;
   double dfdx_ = x_ != 0.0 ? (cos(x_) - sin(x_)) / x_ : 0.0;
-  return precomputed_gradients(f, {x.vi_}, {dfdx_});
+  return var(new precomp_v_vari(f, x.vi_, dfdx_));
 }
 
