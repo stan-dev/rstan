26f8a29aa0a1e084b8ebd93056bd738319d6eb42
Ben Goodrich <goodrich.ben@gmail.com>
2018-11-04 07:02:24 -0500

    introduce and use get_all_makefile_paths()

--- rstan/rstan/R/makefile_op.R
+++ rstan/rstan/R/makefile_op.R
@@ -17,14 +17,7 @@
 
 ###  deal with the optimization level for c++ compilation 
 
-get_makefile_txt <- function() { 
-  # get the all makefile content used for R CMD SHLIB 
-  # The order or files to look for are following the code 
-  # of function .SHLIB in packages tools/R/install.R 
-  # 
-  # Return: 
-  #   a character vector in which each element is a line of 
-  #   the makefile 
+get_all_makefile_paths <- function() {
   out <- system2(file.path(Sys.getenv("R_HOME"), "bin", "R"),
                  args = "CMD SHLIB --dry-run", stdout = TRUE)
   out <- grep("SHLIB", out, value = TRUE)[1]
@@ -33,6 +26,18 @@ get_makefile_txt <- function() {
   makefiles <- gsub("[[:space:]]*$", "", makefiles)
   makefiles <- gsub('\"', '', makefiles)
   makefiles <- makefiles[file.exists(makefiles)]
+  return(makefiles)  
+}
+
+get_makefile_txt <- function() { 
+  # get the all makefile content used for R CMD SHLIB 
+  # The order or files to look for are following the code 
+  # of function .SHLIB in packages tools/R/install.R 
+  # 
+  # Return: 
+  #   a character vector in which each element is a line of 
+  #   the makefile 
+  makefiles <- get_all_makefile_paths()
   do.call(c, lapply(makefiles, function(f) readLines(f, warn = FALSE))) 
 }
 
@@ -71,23 +76,8 @@ last_makefile <- function() {
   # This function return the last one, where we can set flags 
   # to overwrite what is set before. 
   # 
-  WINDOWS <- .Platform$OS.type == "windows"
-  rarch <- Sys.getenv("R_ARCH") # unix only
-  if (WINDOWS && nzchar(.Platform$r_arch))
-    rarch <- paste0("/", .Platform$r_arch)
-
-  if (WINDOWS) { 
-    if (rarch == "/x64") {
-       f <- path.expand("~/.R/Makevars.win64") 
-       if (file.exists(f)) return(f) 
-    }
-    f <- path.expand("~/.R/Makevars.win") 
-    if (file.exists(f)) return(f) 
-    return(path.expand("~/.R/Makevars"))
-  }
-  f <- path.expand(paste("~/.R/Makevars", Sys.getenv("R_PLATFORM"), sep = "-"))
-  if (file.exists(f)) return(f) 
-  return(path.expand("~/.R/Makevars"))
+  makefiles <- get_all_makefile_paths()
+  return(tail(makefiles, n = 1L))
 } 
 
 set_makefile_flags <- function(flags) { 
