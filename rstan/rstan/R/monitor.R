fc813accf0e4306fb796235da9f875a485247f93
Paul Buerkner <paul.buerkner@gmail.com>
2019-03-21 00:42:46 +0100

    update 'monitor' and related functions

--- rstan/rstan/R/monitor.R
+++ rstan/rstan/R/monitor.R
@@ -42,10 +42,10 @@ autocovariance <- function(y) {
   M <- fft_next_good_size(N)
   Mt2 <- 2 * M
   yc <- y - mean(y)
-  yc <- c(yc, rep.int(0, Mt2-N))
+  yc <- c(yc, rep.int(0, Mt2 - N))
   transform <- fft(yc)
   ac <- fft(Conj(transform) * transform, inverse = TRUE)
-  ac <- Re(ac)[1:N]/(N*2*seq(N, 1, by = -1))
+  ac <- Re(ac)[1:N] / (N * 2 * seq(N, 1, by = -1))
   ac
 }
 
@@ -53,43 +53,78 @@ autocorrelation <- function(y) {
   # Compute autocorrelation estimates for every lag for the specified
   # input sequence using a fast Fourier transform approach.
   ac <- autocovariance(y)
-  ac <- ac/ac[1]
+  ac <- ac / ac[1]
+}
+
+z_scale <- function(x) {
+  S <- length(x)
+  r <- rank(x, ties.method = 'average')
+  z <- qnorm((r - 1 / 2) / S)
+  if (!is.null(dim(x))) {
+    # output should have the input dimension
+    z <- array(z, dim = dim(x), dimnames = dimnames(x))
+  }
+  z
+}
+
+u_scale <- function(x) {
+  S <- length(x)
+  r <- rank(x, ties.method = 'average')
+  u <- (r - 1 / 2) / S
+  if (!is.null(dim(x))) {
+    # output should have the input dimension
+    u <- array(u, dim = dim(x), dimnames = dimnames(x))
+  }
+  u
+}
+
+r_scale <- function(x) {
+  S <- length(x)
+  r <- rank(x, ties.method = 'average')
+  if (!is.null(dim(x))) {
+    # output should have the input dimension
+    r <- array(r, dim = dim(x), dimnames = dimnames(x))
+  }
+  r
 }
 
 ess_rfun <- function(sims) {
   # Compute the effective sample size for samples of several chains 
   # for one parameter; see the C++ code of function  
   # effective_sample_size in chains.cpp 
-  # 
   # Args:
-  #   sims: a 2-d array _without_ warmup samples (# iter * # chains) 
-  # 
-  if (is.vector(sims)) dim(sims) <- c(length(sims), 1)
+  #   sims: a 2D array _without_ warmup samples (# iter * # chains) 
+  # Returns:
+  #   A single numeric value
+  if (is.vector(sims)) {
+    dim(sims) <- c(length(sims), 1)
+  }
   chains <- ncol(sims)
   n_samples <- nrow(sims)
   
-  acov <- lapply(1:chains, 
-                 FUN = function(i) autocovariance(sims[,i])) 
+  acov <- lapply(seq_len(chains), function(i) autocovariance(sims[, i])) 
   acov <- do.call(cbind, acov)
   chain_mean <- apply(sims, 2, mean)
-  mean_var <- mean(acov[1,]) * n_samples / (n_samples - 1) 
+  mean_var <- mean(acov[1, ]) * n_samples / (n_samples - 1) 
   var_plus <- mean_var * (n_samples - 1) / n_samples
   if (chains > 1) 
     var_plus <- var_plus + var(chain_mean)
+  
   # Geyer's initial positive sequence
   rho_hat_t <- rep.int(0, n_samples)
   t <- 0
   rho_hat_even <- 1;
-  rho_hat_t[t+1] <- rho_hat_even
-  rho_hat_odd <- 1 - (mean_var - mean(acov[t+2, ])) / var_plus
-  rho_hat_t[t+2] <- rho_hat_odd
+  rho_hat_t[t + 1] <- rho_hat_even
+  rho_hat_odd <- 1 - (mean_var - mean(acov[t + 2, ])) / var_plus
+  rho_hat_t[t + 2] <- rho_hat_odd
   t <- 2  
-  while (t < nrow(acov)-1 && !is.nan(rho_hat_even + rho_hat_odd) && (rho_hat_even + rho_hat_odd > 0)) {
-    rho_hat_even = 1 - (mean_var - mean(acov[t+1, ])) / var_plus
-    rho_hat_odd = 1 - (mean_var - mean(acov[t+2, ])) / var_plus
+  while (t < nrow(acov) - 1 && !is.nan(rho_hat_even + rho_hat_odd) && 
+         (rho_hat_even + rho_hat_odd > 0)) {
+    rho_hat_even = 1 - (mean_var - mean(acov[t + 1, ])) / var_plus
+    rho_hat_odd = 1 - (mean_var - mean(acov[t + 2, ])) / var_plus
     if ((rho_hat_even + rho_hat_odd) >= 0) {
-      rho_hat_t[t+1] <- rho_hat_even
-      rho_hat_t[t+2] <- rho_hat_odd
+      rho_hat_t[t + 1] <- rho_hat_even
+      rho_hat_t[t + 2] <- rho_hat_odd
     }
     t <- t + 2
   }
@@ -109,95 +144,195 @@ ess_rfun <- function(sims) {
   ess 
 } 
 
-split_rhat_rfun <- function(sims) {
-  # Compute the split rhat for the diagnostics of converging; 
-  # see the C++ code of split_potential_scale_reduction in chains.cpp.  
-  # 
+tail_ess <- function(sims) {
+  I05 <- sims <= quantile(sims, 0.05)
+  q05_ess <- ess_rfun(z_scale(split_chains(I05)))
+  I95 <- sims <= quantile(sims, 0.95)
+  q95_ess <- ess_rfun(z_scale(split_chains(I95)))
+  min(q05_ess, q95_ess)
+}
+
+rhat_rfun <- function(sims) {
+  # Compute the rhat convergence diagnostic for a single parameter
+  # For split-rhat, just call this with splitted chains
   # Args:
-  #   sims: a 2-d array _without_ warmup samples (# iter * # chains) 
-  # 
-  # Note: 
-  #   The R function wrapping the C++ implementation is defined 
-  #   in chains.R with name rstan_splitrhat2_cpp 
-  if (is.vector(sims)) dim(sims) <- c(length(sims), 1)
+  #   sims: a 2D array _without_ warmup samples (# iter * # chains) 
+  # Returns:
+  #   A single numeric value
+  if (is.vector(sims)) {
+    dim(sims) <- c(length(sims), 1)
+  }
   chains <- ncol(sims)
   n_samples <- nrow(sims)
-  half_n <- floor(n_samples / 2)
-  # cat("n_samples=", n_samples, "\n"); cat("chains=", chains, "\n")
-  # cat("half_n=", half_n, "\n")
-  idx_2nd <- n_samples - half_n + 1
-  
-  split_chain_mean <- numeric(chains * 2)
-  split_chain_var <- numeric(chains * 2)
-  
-  for (i in 1:chains) {
-    split_chain_mean[i] <- mean(sims[1:half_n, i])
-    split_chain_var[i] <- var(sims[1:half_n, i])
-    split_chain_mean[chains + i] <- mean(sims[idx_2nd:n_samples, i])
-    split_chain_var[chains + i] <- var(sims[idx_2nd:n_samples, i])
+  chain_mean <- numeric(chains)
+  chain_var <- numeric(chains)
+  for (i in seq_len(chains)) {
+    chain_mean[i] <- mean(sims[, i])
+    chain_var[i] <- var(sims[, i])
   } 
-  var_between <- half_n * var(split_chain_mean)
-  var_within <- mean(split_chain_var) 
-  sqrt((var_between/var_within + half_n -1)/half_n)
+  var_between <- n_samples * var(chain_mean)
+  var_within <- mean(chain_var) 
+  sqrt((var_between/var_within + n_samples - 1) / n_samples)
 } 
 
-monitor <- function(sims, warmup = floor(dim(sims)[1] / 2), 
-                    probs = c(0.025, 0.25, 0.50, 0.75, 0.975), 
-                    digits_summary = 1, 
-                    print = TRUE, ...) { 
-  # print the summary for a general simulation results 
-  # of 3-d array: # iter * # chains * # parameters 
+quantile_mcse <- function(sims, prob) {
+  # compute Markov-chain SE of quantiles for a single parameter
+  # prob must be a single probability for the quantile of interest
+  if (is.vector(sims)) {
+    dim(sims) <- c(length(sims), 1)
+  }
+  I <- sims <= quantile(sims, prob)
+  Seff <- ess_rfun(z_scale(split_chains(I)))
+  p <- c(0.1586553, 0.8413447, 0.05, 0.95)
+  a <- qbeta(p, Seff * prob + 1, Seff * (1 - prob) + 1)
+  ssims <- sort(sims)
+  S <- length(ssims)
+  th1 <- ssims[max(round(a[1] * S), 1)]
+  th2 <- ssims[min(round(a[2] * S), S)]
+  mcse <- (th2 - th1) / 2
+  th1 <- ssims[max(round(a[3] * S), 1)]
+  th2 <- ssims[min(round(a[4] * S), S)]
+  data.frame(mcse = mcse, Q05 = th1, Q95 = th2, Seff = Seff)
+}
+
+split_chains <- function(sims) {
+  # split Markov chains
   # Args:
-  #   sims: a 3-d array described above 
+  #   sims: a 2D array of samples (# iter * # chains) 
+  if (is.vector(sims)) {
+    dim(sims) <- c(length(sims), 1)
+  }
+  niter <- dim(sims)[1]
+  half <- niter / 2
+  cbind(sims[1:floor(half), ], sims[ceiling(half + 1):niter, ])
+}
+
+is_constant <- function(x, tol = .Machine$double.eps) {
+  abs(max(x) - min(x)) < tol
+}
+
+monitor <- function(sims, warmup = 0, probs = c(0.05, 0.50, 0.95), 
+                    se = FALSE, print = TRUE, digits = 1, ...) { 
+  # print a summary for general simulation results 
+  # of 3D array: # iter * # chains * # parameters 
+  # Args:
+  #   sims: a 3D array described above or a stanfit object
   #   warmup: the number of iterations used for warmup 
   #   probs: probs of summarizing quantiles 
-  #   print: print out the results
-  # 
+  #   se: include MCSEs in the output
   # Return: 
-  #   A summary array  
-  dim_sims <- dim(sims)
-  if (is.null(dim_sims))
-    dim(sims) <- c(length(sims), 1, 1)
-  if (length(dim_sims) == 2)
-    dim(sims) <- c(dim_sims, 1)
-  if (length(dim_sims) > 3) 
-    stop("'sims' has more than 3 dimensions")
-  dim_sims <- dim(sims)
-  if (warmup > dim_sims[1])
-    stop("warmup is larger than the total number of iterations")
-  if (is(sims, "stanfit")) {
-    warmup <- 0L
+  #   A summary matrix
+  if (inherits(sims, "stanfit")) {
+    chains <- sims@sim$chains
+    iter <- sims@sim$iter
+    warmup <- sims@sim$warmup
+    parnames <- names(sims)
     sims <- as.array(sims)
+  } else {
+    dim_sims <- dim(sims)
+    if (is.null(dim_sims)) {
+      dim(sims) <- c(length(sims), 1, 1) 
+    } else if (length(dim_sims) == 2) {
+      dim(sims) <- c(dim_sims, 1)
+    } else if (length(dim_sims) > 3) {
+      stop("'sims' has more than 3 dimensions") 
+    }
+    parnames <- dimnames(sims)[[3]]
+    if (is.null(parnames)) {
+      parnames <- paste0("V", seq_len(dim(sims)[3]))
+    }
+    iter <- dim(sims)[1]
+    chains <- dim(sims)[2]
+    if (warmup > dim(sims)[1]) {
+      stop("warmup is larger than the total number of iterations")
+    }
+    if (warmup >= 1) {
+      sims <- sims[-seq_len(warmup), , , drop = FALSE] 
+    }
   }
-  dimnames_sims <- dimnames(sims)
-  parnames <- dimnames_sims[[3]]
-  num_par <- dim_sims[3]
   
-  if (is.null(parnames)) parnames <- paste0("V", 1:num_par)
-  sims_wow <- if (warmup >= 1) apply(sims, c(2, 3), FUN = function(x) x[-(1:warmup)]) else sims 
-  m <- apply(sims_wow, 3, mean)
-  sd <- sapply(1:num_par, FUN = function(i) sd(as.vector(sims_wow[,,i]))) 
-  quan <- lapply(1:num_par, FUN = function(i) quantile(sims_wow[,,i], probs = probs))
-  probs_str <- names(quan[[1]])
-  quan <- do.call(rbind, quan)
-  rhat <- sapply(1:num_par, FUN = function(i) split_rhat_rfun(sims_wow[,,i]))
-  ess <- sapply(1:num_par, FUN = function(i) ess_rfun(sims_wow[,,i]))
-  sem <- sd / sqrt(ess)
+  mcse_quan_fun <- function(p, sims) quantile_mcse(sims, p)$mcse
+  out <- vector("list", length(parnames))
+  out <- setNames(out, parnames)
+  # loop over parameters
+  for (i in seq_along(out)) {
+  	sims_i <- sims[, , i]
+  	valid <- all(is.finite(sims_i))
+  	quan <- unname(quantile(sims_i, probs = probs))
+  	mcse_quan <- sapply(probs, mcse_quan_fun, sims_i)
+  	
+  	zsims_split <- z_scale(split_chains(sims_i))
+  	zsplit_rhat <- rhat_rfun(zsims_split)
+  	bulk_ess <- round(ess_rfun(zsims_split))
+  	tail_ess <- round(tail_ess(sims_i))
+  	
+  	sims_folded <- abs(sims_i - median(sims_i))
+  	zsims_folded_split <- z_scale(split_chains(sims_folded))
+  	zfsplit_rhat <- rhat_rfun(zsims_folded_split)
+  	rhat <- max(zsplit_rhat, zfsplit_rhat)
+  	
+  	ess <- ess_rfun(sims_i)
+  	mean <- mean(sims_i)
+  	sd <- sd(sims_i)
+  	mcse_mean <- sd / sqrt(ess)
+  	# mcse_sd assumes normality of sims and uses Stirling's approximation
+  	# min of ess for sims and sims^2 is used instead of ess
+  	ess2 <- ess_rfun(sims_i^2)
+  	essmin <- min(c(ess,ess2))
+  	fac_mcse_sd <- sqrt(exp(1) * (1 - 1 / essmin)^(essmin - 1) - 1)
+  	mcse_sd <- sd * fac_mcse_sd
+  	
+  	out[[i]] <- c(
+  		valid, quan, mean, sd, mcse_quan, mcse_mean,
+  		mcse_sd, rhat, bulk_ess, tail_ess
+  	)
+  }
   
-  summary <- cbind(m, sem, sd, quan, ess, rhat)
-  colnames(summary) <- c("mean", "se_mean", "sd", probs_str, "n_eff", "Rhat")
-  rownames(summary) <- parnames 
+  out <- do.call(rbind, out)
+  str_quan <- paste0("Q", probs * 100)
+  str_mcse_quan <- paste0("MCSE_", str_quan)
+  colnames(out) <- c(
+  	"valid", str_quan, "Mean", "SD", str_mcse_quan, 
+  	"MCSE_Mean", "MCSE_SD", "Rhat", "Bulk_ESS", "Tail_ESS"
+  )
+  rownames(out) <- parnames
+  
+  # replace NAs with appropriate values if draws are valid
+  S <- prod(dim(sims)[1:2])
+  valid <- out[, 1]
+  out <- out[, -1, drop = FALSE]
+  out[valid & !is.finite(out[, "Rhat"]), "Rhat"] <- 1
+  out[valid & !is.finite(out[, "Bulk_ESS"]), "Bulk_ESS"] <- S
+  out[valid & !is.finite(out[, "Tail_ESS"]), "Tail_ESS"] <- S
+  SE_vars <- colnames(out)[grepl("^SE_", colnames(out))]
+  for (v in SE_vars) {
+  	out[valid & !is.finite(out[, v]), v] <- 0
+  }
+  
   if (print) {
-    cat("Inference for the input samples (")
-    cat(dim_sims[2], " chains: each with iter=", dim_sims[1], "; warmup=", warmup, "):\n\n", sep = "")
-    # round n_eff to integers
-    summary[, 'n_eff'] <- round(summary[, 'n_eff'], 0)
-    print(round(summary, digits_summary), ...)
-    
-    cat("\nFor each parameter, n_eff is a crude measure of effective sample size,\n", 
-        "and Rhat is the potential scale reduction factor on split chains (at \n",
-        "convergence, Rhat=1).\n", sep = '')
-  } 
-  invisible(summary) 
+  	# amended output for pretty printing
+  	tmp <- out
+  	if (!se) {
+  		tmp <- tmp[, !grepl("^MCSE_", colnames(tmp))]
+  	}
+  	decimal_places <- max(1, digits - 1)
+  	tmp[, "Rhat"] <- round(tmp[, "Rhat"], digits = max(2, decimal_places))
+  	estimates <- setdiff(names(tmp), c("Rhat", "Bulk_ESS", "Tail_ESS"))
+  	tmp[, estimates] <- round(tmp[, estimates], digits = decimal_places)
+  	# add a space between summary and convergence estimates
+  	colnames(tmp)[colnames(tmp) %in% "Rhat"] <- " Rhat"
+  	cat(
+  		"Inference for the input samples (", chains, 
+  		" chains: each with iter = ", iter, 
+  		"; warmup = ", warmup, "):\n\n", sep = ""
+  	)
+  	print(tmp, digits = digits, ...)
+  	cat(
+  		"\nFor each parameter, Bulk_ESS and Tail_ESS are crude measures of \n",
+  		"effective sample size for bulk and tail quantities respectively (good values is \n",
+  		"ESS > 400), and Rhat is the potential scale reduction factor on rank normalized\n",
+  		"split chains (at convergence, Rhat = 1).\n", sep = ""
+  	)
+  }
+  invisible(out) 
 } 
-
