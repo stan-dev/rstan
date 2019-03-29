45ba89185cb5fc591cb1149adbc23703bb57350d
Ben Goodrich <goodrich.ben@gmail.com>
2019-03-29 18:54:47 -0400

    expose_stan_functions: use R's PRNG
    also use Rcpp's output stream
    
    make corresponding changes in other files

--- rstan/rstan/R/get_stream.R
+++ rstan/rstan/R/get_stream.R
@@ -1,4 +1,6 @@
 get_stream <- function() {
+  message("The 'get_stream' function is deprecated and does not do anything useful.",
+          "The output stream is now handled by Rcpp.")
   return(.Call('get_stream_'))
 }
 
