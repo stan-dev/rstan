7319cf56c244df6bc1797ab3e8947172de8978ae
Ben Goodrich <goodrich.ben@gmail.com>
2019-11-10 16:52:34 -0500

    add zzz.R file that deals with the plugin

---
+++ StanHeaders/R/zzz.R
@@ -0,0 +1,16 @@
+.onAttach(libname, pkgname) {
+  pkg_cppflags <- Sys.getenv("PKG_CPPFLAGS")
+  plugin <- system.file("include", "stan", "math", "prim", "mat", "fun", "Eigen.hpp",
+                        package = "StanHeaders", mustWork = TRUE)
+  Sys.setenv(PKG_CPPFLAGS = paste(pkg_cppflags, "-include", plugin))
+  return(invisible(NULL))  
+}
+
+.onDetach(libpath) {
+  pkg_cppflags <- Sys.getenv("PKG_CPPFLAGS")
+  plugin <- system.file("include", "stan", "math", "prim", "mat", "fun", "Eigen.hpp",
+                        package = "StanHeaders", mustWork = TRUE)
+  pkg_cppflags <- sub(paste("-include", plugin), "", pkg_cppflags, fixed = TRUE)
+  Sys.setenv(PKG_CPPFLAGS = pkg_cppflags)
+  return(invisible(NULL))
+}
