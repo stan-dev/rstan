114404109b9f502f3d23640dc4933a8ecf33bee5
Sebastian Weber <sebastian.weber@novartis.com>
2020-07-02 17:17:38 +0200

    create models separatley and keep them in memory

--- rstan/rstan/R/Rcpp_module_def.R
+++ rstan/rstan/R/Rcpp_module_def.R
@@ -23,35 +23,26 @@ get_Rcpp_module_def_code <- function(model_name) {
 struct stan_model_holder {
     stan_model_holder(rstan::io::rlist_ref_var_context rcontext,
                       unsigned int random_seed)
-    : rcontext_(rcontext), random_seed_(random_seed),
-      model_raw_(new stan_model(rcontext_, random_seed_)),
-      model_(model_raw_, false)
+    : rcontext_(rcontext), random_seed_(random_seed)
      {
-       R_PreserveObject(model_);
      }
 
    //stan::math::ChainableStack ad_stack;
    rstan::io::rlist_ref_var_context rcontext_;
    unsigned int random_seed_;
-   stan::model::model_base* model_raw_;
-   Rcpp::XPtr<stan::model::model_base> model_;
 };
 
 Rcpp::XPtr<stan::model::model_base> model_ptr(stan_model_holder* smh) {
-  return smh->model_;
+  Rcpp::XPtr<stan::model::model_base> model_instance(new stan_model(smh->rcontext_, smh->random_seed_), true);
+  return model_instance;
 }
 
 Rcpp::XPtr<rstan::stan_fit_base> fit_ptr(stan_model_holder* smh) {
-  return Rcpp::XPtr<rstan::stan_fit_base>(new rstan::stan_fit(smh->model_, smh->random_seed_), true);
+  return Rcpp::XPtr<rstan::stan_fit_base>(new rstan::stan_fit(model_ptr(smh), smh->random_seed_), true);
 }
 
 std::string model_name(stan_model_holder* smh) {
-  return smh->model_.get()->model_name();
-}
-
-void finalize_stan_model_holder(stan_model_holder* smh) {
-//   R_ReleaseObject(smh->model_raw_);
-   delete smh->model_raw_;
+  return model_ptr(smh).get()->model_name();
 }
 
 RCPP_MODULE(stan_fit4%model_name%_mod){
@@ -60,7 +51,6 @@ RCPP_MODULE(stan_fit4%model_name%_mod){
   .method("model_ptr", &model_ptr)
   .method("fit_ptr", &fit_ptr)
   .method("model_name", &model_name)
-  .finalizer(&finalize_stan_model_holder)
   ;
 }
 '
