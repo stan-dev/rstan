84841bb4d5bf71a7456ffa65296f19cff4f3b3ba
Kevin Ushey <kevinushey@gmail.com>
2021-04-14 12:04:52 -0700

    use new RcppParallel API for resolving TBB when available

--- StanHeaders/R/Flags.R
+++ StanHeaders/R/Flags.R
@@ -7,9 +7,25 @@ CxxFlags <- function(as_character = FALSE) {
 }
 
 LdFlags <- function(as_character = FALSE) {
-  TBB <- system.file("lib", .Platform$r_arch, package = "RcppParallel", mustWork = TRUE)
+
+  # if RcppParallel provides the requisite API for querying
+  # the TBB library path, use it
+  TBB <- NULL
+  if (requireNamespace("RcppParallel", quietly = TRUE)) {
+    RcppParallel <- asNamespace("RcppParallel")
+    if (is.function(RcppParallel$tbbLibraryPath))
+      TBB <- RcppParallel$tbbLibraryPath()
+  }
+
+  # Otherwise, find it in the default location (for older RcppParallel releases)
+  if (is.null(TBB))
+    TBB <- system.file("lib", .Platform$r_arch, package = "RcppParallel", mustWork = TRUE)
+  
   PKG_LIBS <- paste0("-L", shQuote(TBB), " -Wl,-rpath,", shQuote(TBB), " -ltbb -ltbbmalloc")
-  if (isTRUE(as_character)) return(PKG_LIBS)
+  if (isTRUE(as_character))
+    return(PKG_LIBS)
+
   cat(PKG_LIBS, " ")
   return(invisible(NULL))
+
 }
